a0da63f55f03e047724753a113b856b886e3b5a6
Divyesh Bhesaniya <68001924+Divyesh4683@users.noreply.github.com>
2025-12-03 17:03:52 -0500

    Update analytics-integration.js

--- web/player-update/analytics-integration.js
+++ web/player-update/analytics-integration.js
@@ -1,5 +1,5 @@
 // analytics-integration.js
-const ANALYTICS_API = 'https://n3r8pu738e.execute-api.us-east-1.amazonaws.com/prod/analytics';
+const ANALYTICS_API = 'https://ANALYTICS_API_ID.execute-api.us-east-1.amazonaws.com/prod/analytics';
 
 async function loadAnalytics() {
     try {
